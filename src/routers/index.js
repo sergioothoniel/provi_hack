a5b197146fbe9cfd06df50ac23dd867cbeeec308
maykonx22 <maykon.x23@gmail.com>
2022-05-01 01:37:30 -0400

    feat: Criação Modal Menu

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,16 +1,26 @@
 import { Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import { Perfil } from "../pages/Perfil";
 import Register from "../pages/Register";
 
-function Routers(){
-    return(
-        <Switch>
-            <Route path={"/login"}><Login/></Route>
-            <Route path={"/register"}><Register/></Route>
-            <Route path={"/"}><Home/></Route>
-        </Switch>
-    )
+function Routers() {
+  return (
+    <Switch>
+      <Route exact path={"/"}>
+        <Home />
+      </Route>
+      <Route path={"/login"}>
+        <Login />
+      </Route>
+      <Route path={"/register"}>
+        <Register />
+      </Route>
+      <Route path={"/perfil"}>
+        <Perfil />
+      </Route>
+    </Switch>
+  );
 }
 
-export default Routers
+export default Routers;
